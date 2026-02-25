f0a64add4bc55644c0c22f37a34d6526bdfde068
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-25 16:41:21 +0800

    Add random rgb color on hover

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector("#container");
 const resizeBtn = document.querySelector("#resizeBtn");
 
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 function makeGrid(size) {
   container.innerHTML = "";
 
@@ -15,7 +23,7 @@ function makeGrid(size) {
     square.style.height = `${squareSize}px`;
 
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
+      square.style.backgroundColor = randomColor();
     });
 
     container.appendChild(square);
