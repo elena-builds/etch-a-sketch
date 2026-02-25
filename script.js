089955f6d353931c66ac54e4aeacbdaeb9629cee
Elena <260600984+elena-builds@users.noreply.github.com>
2026-02-25 16:23:54 +0800

    Generate 16*16 grid with javascript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("script loaded");
+const container = document.querySelector("#container");
+
+function makeGrid(size) {
+  container.innerHTML = "";
+
+  const totalSquares = size * size;
+  const squareSize = 960 / size;
+
+  for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
+    container.appendChild(square);
+  }
+}
+
+makeGrid(16);
